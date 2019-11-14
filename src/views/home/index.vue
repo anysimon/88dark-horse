087e34e1988bbf73b3b981e294846e859fb700d2
Simon <3093396327@qq.com>
2019-11-14 15:16:30 +0800

    黑马头条PC-主页模块-左侧导航菜单

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,13 +1,27 @@
 <template>
-  <div>主页</div>
+  <el-container>
+    <!-- 左侧导航 -->
+    <el-aside></el-aside>
+    <!-- 右侧模块 -->
+    <el-container>
+      <el-header>Header</el-header>
+      <el-main>
+        <!-- 二级路由容器 -->
+        main
+        <router-view>main</router-view>
+      </el-main>
+    </el-container>
+  </el-container>
 </template>
 
 <script>
+import leftNav from './left-Nav'
 export default {
-
+  components: {
+    'el-aside': leftNav
+  }
 }
 </script>
 
-<style>
-
+<style lang='less' scoped>
 </style>
---
+++ src/views/home/left-Nav.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="navLeft">
+    <div class="logo">
+      <img src="../../assets/img/logo_admin.png" alt />
+    </div>
+    <el-menu background-color="#323745" text-color="#adafb5" active-text-color="#ffd04b" style="width:201px">
+      <!-- 一级列表 -->
+      <el-menu-item index="1">
+        <span slot="title">首页</span>
+      </el-menu-item>
+      <!-- 二级菜单 -->
+      <el-submenu index="2">
+        <template slot="title">
+          <span>内容管理</span>
+        </template>
+        <el-menu-item-group>
+          <el-menu-item index="2-1">发布文章</el-menu-item>
+          <el-menu-item index="2-2">内容列表</el-menu-item>
+          <el-menu-item index="2-3">评论列表</el-menu-item>
+          <el-menu-item index="2-4">素材管理</el-menu-item>
+        </el-menu-item-group>
+      </el-submenu>
+      <!-- 二级菜单 -->
+      <el-submenu index="3">
+        <template slot="title">粉丝管理</template>
+        <el-menu-item-group>
+          <el-menu-item index="3-1">图文数据</el-menu-item>
+          <el-menu-item index="3-2">粉丝概况</el-menu-item>
+          <el-menu-item index="3-3">粉丝画像</el-menu-item>
+          <el-menu-item index="3-4">粉丝列表</el-menu-item>
+        </el-menu-item-group>
+      </el-submenu>
+      <!-- 一级列表 -->
+      <el-menu-item index="4">
+        <span slot="title">账户信息</span>
+      </el-menu-item>
+    </el-menu>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang='less' scoped>
+.navLeft {
+  width: 200px;
+  height: 100vh;
+  background-color: #323745;
+  .logo {
+    background-color: #2e2f32;
+    padding: 15px;
+    text-align: center;
+    img {
+      height: 30px;
+    }
+  }
+}
+</style>
